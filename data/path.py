d411371932f92c5d28b8d11230cda3feccaf51f6
Eli <elibloch2903@gmail.com>
2025-09-07 12:58:06 +0300

    path

--- data/path.py
+++ data/path.py
@@ -5,3 +5,7 @@ dir = Path(r"C:\Users\elibl\podcasts-20250907T074357Z-1-001\podcasts")
 file = sorted(dir.glob(".wav"))
 for f in file:
     pass
+
+# class path:
+#     def __init__(self, path):
+#         self.path = path
