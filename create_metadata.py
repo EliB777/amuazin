30cd3c7c61b88d7da88b44066deb94e79c8c70fd
Eli <elibloch2903@gmail.com>
2025-09-07 16:29:20 +0300

    create_metadata and print after fixing

--- create_metadata.py
+++ create_metadata.py
@@ -1,19 +1,23 @@
 from pathlib import Path
-import wave, contextlib
+import wave, contextlib, json, time
+from datetime import datetime
 
 dir = Path(r"C:\Users\elibl\podcasts-20250907T074357Z-1-001\podcasts")
-
 file = dir.glob("*.wav")
-
+text = []
 
 for f in file:
-
     stat = f.stat()
     with contextlib.closing(wave.open(str(f), 'rb')):
         metadata = {
             "name": f.name,
             "path": str(f.resolve()),
             "size": stat.st_size,
-            # "datetime": datetime. צריך תיקון
+            "datetime": datetime.fromtimestamp(stat.st_mtime).isoformat(),
+            # "datetime": time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime(stat.st_mtime))
         }
+
+    msg = json.dumps(metadata, ensure_ascii=False)
+    text.append(msg)
+    print(msg)
 
